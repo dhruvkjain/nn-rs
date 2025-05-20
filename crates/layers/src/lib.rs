e7f91a97e5bc694e3369b2b7199dcb44a9e4641d
Dhruv-K-Jain <138356622+Dhruv-K-Jain@users.noreply.github.com>
2025-05-20 07:37:33 +0530

    feat: modularized and generalized whole model

--- crates/layers/src/lib.rs
+++ crates/layers/src/lib.rs
@@ -1,62 +1,14 @@
-use layer::*;
-use ndarray::Array2;
+use ndarray::{Array1, Array2};
+
+pub trait Propagate {
+    fn forward(&mut self, input:&Array2<f32>) -> Array2<f32>;
+    fn backward(&mut self, grad_output: &Array2<f32>) -> Array2<f32>;
+
+    // Optional for trainable layers only
+    fn params_grads(&mut self) -> Option<(&mut Array2<f32>, &mut Array2<f32>, &mut Array1<f32>, &mut Array1<f32>)> {
+        None
+    }
+}
+
 pub mod layer;
-
-pub struct NN<S: Loss, O: Optimizer> {
-    pub layers: Vec<Box<dyn Propagate>>,
-    pub loss_fn: S,
-    pub optim: O,
-}
-
-impl<S: Loss, O: Optimizer> NN<S, O> {
-    /// Forward pass through all layers.
-    pub fn forward_all(&mut self, x: &Array2<f32>) -> Array2<f32> {
-        let mut z = x.clone();
-        for layer in  self.layers.iter_mut(){
-            z = layer.forward(&z);
-        }
-        z
-    }
-
-    /// Backward pass: loss gradient → all layers (in reverse)
-    pub fn backward_all(&mut self, grad_loss: &Array2<f32>) {
-        let mut grad = grad_loss.clone();
-        for layer in self.layers.iter_mut().rev() {
-            grad = layer.backward(&grad);
-        }
-    }
-
-    /// Single training step on batch (inputs, targets)
-    pub fn train_step(&mut self, x: &Array2<f32>, y: &Array2<f32>) -> f32 {
-        // Forward
-        let preds = self.forward_all(x);
-        // Loss
-        let loss = self.loss_fn.forward(&preds, y);
-        let grad_loss = self.loss_fn.backward(&preds);
-        // Backward
-        self.backward_all(&grad_loss);
-        // Collect all params and grads
-        let mut all_weights = Vec::new();
-        let mut all_grad_weights = Vec::new();
-        let mut all_bias = Vec::new();
-        let mut all_grad_bias = Vec::new();
-        for layer in self.layers.iter_mut() {
-            match layer.params_grads() {
-                Some((weights ,grad_weights, bias, grad_bias)) => {
-                    all_weights.push(weights);
-                    all_grad_weights.push(grad_weights);
-                    all_bias.push(bias);
-                    all_grad_bias.push(grad_bias);
-                }
-                None => { 
-                    // println!("Activation Layer weights and params can't be collected") 
-                }
-            }
-        }
-        // Update
-        self.optim.step_weight(&mut all_weights, &mut all_grad_weights);
-        self.optim.step_bias(&mut all_bias, &mut all_grad_bias);
-        loss
-    }
-}
-
+pub mod relu;
---
+++ crates/layers/src/relu.rs
@@ -0,0 +1,22 @@
+use super::Propagate;
+use ndarray::Array2;
+
+pub struct ReLu {
+    cache: Option<Array2<f32>>,
+}
+impl ReLu {
+    pub fn new() -> Self { 
+        ReLu { cache: None } 
+    }
+}
+impl Propagate for ReLu {
+    fn forward(&mut self, input: &Array2<f32>) -> Array2<f32> {
+        let mask = input.mapv(|x| x.max(0.0));
+        self.cache = Some(mask.clone());
+        mask
+    }
+    fn backward(&mut self, grad_output: &Array2<f32>) -> Array2<f32> {
+        let mask = self.cache.as_ref().expect("No cache");
+        grad_output * mask
+    }
+}
